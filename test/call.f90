eb41ff4935dab669bde482861565ee175a2c5c3e
Takuya Miyashita <miyashita@hydrocoast.jp>
2026-02-11 15:23:30 -0800

    convert upper to lower

--- test/call.f90
+++ test/call.f90
@@ -1,43 +1,43 @@
 ! This is a conversion file from FORTRAN77 to Fortran90
-! The original source is available online:
-!   http://www.bosai.go.jp/study/application/dc3d/download/DC3Dmanual.pdf
+! The originAL source is available online:
+!   http://www.bosai.go.jp/study/application/dc3d/download/DC3DmanuAL.pdf
 program test
     implicit none
 
-    real(8) :: X, Y, Z, DEPTH, DIP, AL1, AL2, AW1, AW2, DISL1, DISL2, DISL3, ALPHA
-    real(8) :: UX,UY,UZ,UXX,UYX,UZX,UXY,UYY,UZY,UXZ,UYZ,UZZ
-    integer :: IRET
+    reAL(8) :: x, y, z, depth, dip, AL1, AL2, AW1, AW2, disl1, disl2, disl3, ALpha
+    reAL(8) :: ux,uy,uz,uxx,uyx,uzx,uxy,uyy,uzy,uxz,uyz,uzz
+    integer :: iret
 
-    real(8) :: POT1, POT2, POT3, POT4
+    reAL(8) :: pot1, pot2, pot3, pot4
 
     !----- Check DC3D
-    X=10.0d0
-    Y=20.0d0
-    Z=-30.0d0
-    !Z=30.0d0
-    DEPTH=50.0d0
-    DIP=70.0d0
+    x=10.0d0
+    y=20.0d0
+    z=-30.0d0
+    !z=30.0d0
+    depth=50.0d0
+    dip=70.0d0
     AL1=-80.0d0
     AL2=120.0d0
     AW1=-30.0d0
     AW2=25.0d0
-    DISL1=200.0d0
-    DISL2=-150.0d0
-    DISL3=100.0d0
+    disl1=200.0d0
+    disl2=-150.0d0
+    disl3=100.0d0
     !-----
-    write(*,*) '*** OUTPUT OF DC3D ***'
-    write(*,'(a,2(f8.2,","))') 'DEPTH, DIP =',DEPTH,DIP
+    write(*,*) '*** output of DC3D ***'
+    write(*,'(a,2(f8.2,","))') 'depth, dip =',depth,dip
     write(*,'(a,4(f8.2,","))') 'AL1, AL2, AW1, AW2 =',AL1,AL2,AW1,AW2
-    write(*,'(a,4(f8.2,","))') 'DISL1, DISL2, DISL3 =',DISL1,DISL2,DISL3
-    write(*,'(3(a,f8.2,","))') 'X =',X,' Y =',Y,' Z =',Z
+    write(*,'(a,4(f8.2,","))') 'disl1, disl2, disl3 =',disl1,disl2,disl3
+    write(*,'(3(a,f8.2,","))') 'x =',x,' y =',y,' z =',z
     !-----
-    ALPHA=2.0d0/3.0d0
-    call DC3D(ALPHA,X,Y,Z,DEPTH,DIP, &
-              AL1,AL2,AW1,AW2,DISL1,DISL2,DISL3, &
-              UX,UY,UZ,UXX,UYX,UZX,UXY,UYY,UZY,UXZ,UYZ,UZZ,IRET)
+    ALpha=2.0d0/3.0d0
+    cALl DC3D(ALpha,x,y,z,depth,dip, &
+              AL1,AL2,AW1,AW2,disl1,disl2,disl3, &
+              ux,uy,uz,uxx,uyx,uzx,uxy,uyy,uzy,uxz,uyz,uzz,iret)
     write(*,*)
-    write(*,'(a,I1)') 'IRET = ',IRET
-    write(*,'(a,3(f11.6,","))') 'UX, UY, UZ =',UX,UY,UZ
-    write(*,*) '   ANSWER = -37.8981,    63.1789,    14.9607'
+    write(*,'(a,I1)') 'iret = ',iret
+    write(*,'(a,3(f11.6,","))') 'ux, uy, uz =',ux,uy,uz
+    write(*,*) '   answer = -37.8981,    63.1789,    14.9607'
     stop
 end program test
